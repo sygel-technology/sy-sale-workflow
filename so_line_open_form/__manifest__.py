b6822eb8cb076c562599c55bc7957b418bf2d527
manu <manuel.regidor@sygel.es>
2022-04-26 13:33:04 +0200

    T-2766[ADD]so_line_open_form, abrir form editar linea so

---
+++ so_line_open_form/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Order Line Open Form',
+    'version': '15.0.1.0.0',
+    'author': 'Sygel',
+    'category': 'Sale',
+    'summary': "Open form view when click on sale order line.",
+    'website': 'https://www.sygel.es',
+    'depends': [
+        'sale',
+    ],
+    'data': [
+        'views/sale_views.xml',
+    ],
+}
