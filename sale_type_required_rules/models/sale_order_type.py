07b05a8b38abd0340220f5ebd2286473696df50d
manu <manuel.regidor@sygel.es>
2023-04-11 12:57:06 +0200

    T-4271[ADD]sale_type_required_rules

---
+++ sale_type_required_rules/models/sale_order_type.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import models, fields
+
+
+class SaleOrderTypology(models.Model):
+    _inherit = "sale.order.type"
+
+    required_rule_ids = fields.Many2many(
+        string="Order Required Rules",
+        comodel_name="sale.order.required.rule"
+    )
+    use_required_rules = fields.Boolean(
+        string="Use Required Rules"
+    )
