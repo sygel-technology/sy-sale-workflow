d6d554f7fe9f1f01c9d32f91a8fea9c410056896
angel <angel.garcia@sygel.com>
2023-12-14 09:48:46 +0100

    [ADD] v.16

--- sale_type_required_rules/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright 2023 Manuel Regidor <manuel.regidor@sygel.es>
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from . import sale_order_type
-from . import sale_order
-from . import sale_order_required_rule
