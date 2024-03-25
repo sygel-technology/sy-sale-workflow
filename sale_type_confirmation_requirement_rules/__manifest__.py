3861b6ddf00962a76c4812e0814766d75ad5b9b4
manu <manuel.regidor@sygel.es>
2024-03-25 16:40:18 +0100

    T-5698[ADD]sale_type_confirmation_requirement_rules: reglas que las ventas tienen que cumplir para confirmar

---
+++ sale_type_confirmation_requirement_rules/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'Sale Type Confirmation Requirement Rules',
+    'version': '15.0.1.0.0',
+    "summary": "Required domain conditions when validating a Sale Order.",
+    "website": "https://www.sygel.es",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    'category': 'Sales',
+    'license': 'AGPL-3',
+    'depends': [
+        'sale_order_type',
+        'base_confirmation_requirement_rules',
+    ],
+    'data': [
+        'views/sale_order_type_view.xml',
+        'views/sale_order_requirement_rule_views.xml',
+        'security/ir.model.access.csv',
+    ],
+    'installable': True,
+    'auto_install': False,    
+}
