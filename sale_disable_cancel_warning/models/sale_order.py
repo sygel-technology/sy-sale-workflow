890452ad6f7667baba5f011aa18e42239dce5276
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 11:32:33 +0200

    [T-5944][IMP]sale_disable_cancel_warning,sale_order_line_view_negative_margin,sale_stock_deposit,so_group_stock_user_read,so_line_open_form:pre-commit auto fixes

--- sale_disable_cancel_warning/models/sale_order.py
+++ sale_disable_cancel_warning/models/sale_order.py
@@ -5,9 +5,9 @@ from odoo import models
 
 
 class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+    _inherit = "sale.order"
 
     def action_cancel(self):
-        if True in self.mapped('company_id.disable_cancel_warning'):
+        if True in self.mapped("company_id.disable_cancel_warning"):
             self = self.with_context(disable_cancel_warning=True)
         return super().action_cancel()
