b90bb4eff727eb68d2c4656d639ec8244db70942
sygel <alberto.martinez@sygel.es>
2026-05-22 14:27:52 +0200

    [T-9594][IMP] sale_type_confirmation_requirement_rules: pre-commit auto fixes

--- sale_type_confirmation_requirement_rules/__manifest__.py
+++ sale_type_confirmation_requirement_rules/__manifest__.py
@@ -6,7 +6,7 @@
     "version": "15.0.1.0.1",
     "summary": "Required domain conditions when validating a Sale Order.",
     "website": "https://github.com/sygel-technology/sy-sale-workflow",
-    "author": "Sygel, Odoo Community Association (OCA)",
+    "author": "Sygel",
     "license": "AGPL-3",
     "category": "Sales",
     "depends": [
