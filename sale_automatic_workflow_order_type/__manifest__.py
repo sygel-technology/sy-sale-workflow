080ac6774c2869d148ea427ab98ababcf8984fb4
sygel <alberto.martinez@sygel.es>
2026-05-22 13:13:10 +0200

    [T-9594][MIG] sale_automatic_workflow_order_type: Migration to 18.0

--- sale_automatic_workflow_order_type/__manifest__.py
+++ sale_automatic_workflow_order_type/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Automatic Workflow Order Type",
     "summary": "Sale Automatic Workflow Order Type",
-    "version": "17.0.1.0.0",
+    "version": "18.0.1.0.0",
     "category": "Sales Management",
     "website": "https://github.com/sygel-technology/sy-sale-workflow",
     "author": "Sygel",
