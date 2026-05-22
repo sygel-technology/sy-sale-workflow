37ff4f9fd947f2aace4ff87b1ef1dc6375984afa
sygel <alberto.martinez@sygel.es>
2026-05-22 13:39:37 +0200

    [T-9594][MIG] sale_automatic_workflow_force_invoice: Migration to 18.0

--- sale_automatic_workflow_force_invoice/__manifest__.py
+++ sale_automatic_workflow_force_invoice/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Sale Automatic Workflow Force Invoice",
     "summary": "Force Invoice as an automatic workflow option",
-    "version": "15.0.1.0.0",
+    "version": "18.0.1.0.0",
     "author": "Sygel",
     "website": "https://github.com/sygel-technology/sy-sale-workflow",
     "category": "Sales Management",
